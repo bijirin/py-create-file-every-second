8f1e4bd37fbdf4cbad5ef42f1d83498752f6d40e
bijirin <tsutrisn@gmail.com>
2026-04-23 22:48:58 +0700

    Implement create file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    try:
+        while True:
+            now = datetime.now()
+            file_name = (
+                f"app-{now.hour:02}_"
+                f"{now.minute:02}_"
+                f"{now.second:02}.log"
+            )
+            time_stamp = (
+                    f"{now.year}-"
+                    f"{now.month:02}-"
+                    f"{now.day:02} "
+                    f"{now.hour:02}:"
+                    f"{now.minute:02}:"
+                    f"{now.second:02}"
+            )
+            with open(file_name, "w") as file:
+                file.write(time_stamp)
+            print(time_stamp + " " + file_name)
+            sleep(1)
+    except KeyboardInterrupt:
+        print("Interrupted by user")
 
 
 if __name__ == "__main__":
