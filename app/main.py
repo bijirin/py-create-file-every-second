24711c321668e82954318c13d83d8e15139468d2
bijirin <tsutrisn@gmail.com>
2026-04-23 22:54:16 +0700

    Removed exception handling

--- app/main.py
+++ app/main.py
@@ -3,28 +3,25 @@ from time import sleep  # DO NOT CHANGE THIS IMPORT
 
 
 def main() -> None:
-    try:
-        while True:
-            now = datetime.now()
-            file_name = (
-                f"app-{now.hour:02}_"
-                f"{now.minute:02}_"
-                f"{now.second:02}.log"
-            )
-            time_stamp = (
-                f"{now.year}-"
-                f"{now.month:02}-"
-                f"{now.day:02} "
-                f"{now.hour:02}:"
-                f"{now.minute:02}:"
-                f"{now.second:02}"
-            )
-            with open(file_name, "w") as file:
-                file.write(time_stamp)
-            print(time_stamp + " " + file_name)
-            sleep(1)
-    except KeyboardInterrupt:
-        print("Interrupted by user")
+    while True:
+        now = datetime.now()
+        file_name = (
+            f"app-{now.hour:02}_"
+            f"{now.minute:02}_"
+            f"{now.second:02}.log"
+        )
+        time_stamp = (
+            f"{now.year}-"
+            f"{now.month:02}-"
+            f"{now.day:02} "
+            f"{now.hour:02}:"
+            f"{now.minute:02}:"
+            f"{now.second:02}"
+        )
+        with open(file_name, "w") as file:
+            file.write(time_stamp)
+        print(time_stamp + " " + file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
